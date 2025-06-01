00768248110b7080d2a74d5a36f768007e8a5933
ronjay1 <ronjay@laivdata.com>
2025-06-02 00:29:19 +0900

    WIP : fix conflicts with config files

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -10,12 +10,12 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
-	author: 'Your Name', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
-	paginationSize: 6 // Number of posts per page
+	site: 'http://localhost:4321/', // 로컬 개발용 URL
+	author: 'Joron', // 사이트 작성자
+	title: 'Ronjay DevOps Blog', // 사이트 제목
+	description: 'AWS, Docker, Kubernetes, DevOps를 좋아하는 개발자의 기술 블로그', // 메타 태그에 표시될 설명
+	lang: 'ko',
+	ogLocale: 'ko_KR', 
+	shareMessage: '이 포스트 공유하기', // 소셜 미디어 공유 메시지
+	paginationSize: 6 // 페이지당 포스트 수
 }
